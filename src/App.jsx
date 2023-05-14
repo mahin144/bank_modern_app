3027b4d360cda75013bcacab5a698df799719fe3
mahin144 <mahinibrahim144@gmail.com>
2023-05-13 18:04:10 -0700

    components created

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
-import React from "react";
+import styles from "./style";
 
 const App = () => (
   <div className="bg-primary w-full overflow-hidden">
-    <h1>Hello World!</h1>
+    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>navBar</div>
+    </div>
+
+    <div className={`bg-primary ${styles.flexStart}`}>
+      <div classname={`${styles.boxWidth}`}>Hero</div>
+    </div>
+
+    <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+      <div classname={`${styles.boxWidth}`}>stats</div>
+    </div>
   </div>
 );
 
